8ec05ef5ff1f07a379a6cce3876e26c20927a4cf
Joel Lara <71894655+daw2-lara21@users.noreply.github.com>
2020-12-03 18:05:05 +0100

    Add files via upload

---
+++ N5P101comments.php
@@ -0,0 +1,16 @@
+<?php
+    echo "He recordado el switch";
+    echo "He refrescado el uso de los formularios";
+    echo "He aprendido a crear tablas mediante el echo en php";
+    echo "Actualizar datos al ingresarlos en un formulario";
+    echo "El uso de $_SERVER";
+    echo "Eliminar datos ingresados";
+    echo "Añadir datos";
+    echo "Devolver una cadena con ucfirst";
+    echo "Uso de select";
+    echo "He aprendido los tr en PHP";
+    echo "La  calificación del documento esta vez le doy un 8, la práctica ha sido sencilla pese a que la 1 no la terminé del todo";
+    echo "La explicación del profesor daré un 9 ya que si no fuese por ella no avanzaría";
+    echo "Como alumno, estoy algo estresado entre tantas prácticas, pero voy haciendo";
+    echo "Ninguna por ahora";
+?>
